f5445dce35c117fc75def1705e0c1b8f66b7416d
weilei <X20060@auteltech.net>
2020-09-17 17:22:20 +0800

    change

--- middlewares/router.js
+++ middlewares/router.js
@@ -1,1 +1,10 @@
 const router = new require('koa-router')();
+
+module.exports = function(app) {
+	router.get('/', ctx => ctx.body = 'hello world!');
+	router.get('/api/sms-shop-app-cn/staff/allUsers', ctx => {
+		ctx.body = { "code": 0, "message": "³É¹¦", "data": [{ "createTime": null, "nickname": "autel_dQc86iQTrx", "avatar": "", "shopId": "", "roleList": null, "userId": "1277575454325694465", "username": "autel_dQc86iQTrx" }] };
+	});
+	app.use(router.routes())
+		.use(router.allowedMethods());
+}
